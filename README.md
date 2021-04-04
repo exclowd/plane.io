b95102e7bd00fde47ce8c1e1a0f2b05409839a8e
Kannav Mehta <kmkannavkmehta@gmail.com>
2021-04-05 03:48:57 +0530

    Added Cube animation

---
+++ README.md
@@ -0,0 +1,2 @@
+To run use
+`npm start` 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { Scene } from "three";
 import { PerspectiveCamera } from "three";
 import { WebGLRenderer } from "three";
+import { BoxGeometry, MeshBasicMaterial, Mesh } from "three";
 
 const scene = new Scene();
 const camera = new PerspectiveCamera(
@@ -11,5 +12,22 @@ const camera = new PerspectiveCamera(
 );
 const renderer = new WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
-console.log("hello")
 document.body.appendChild(renderer.domElement);
+
+const geometry = new BoxGeometry();
+const material = new MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.z = 5;
+
+const animate = () => {
+  requestAnimationFrame(animate);
+
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+
+  renderer.render(scene, camera);
+};
+
+animate();
