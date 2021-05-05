749ea9e04d6d4eea76f1d61455d8cbc38f7db91e
Kannav Mehta <kmkannavkmehta@gmail.com>
2021-05-05 13:09:01 +0530

    Added final things

--- src/index.js
+++ src/index.js
@@ -60,7 +60,7 @@ camera.position.set(0, 0, 0);
 camera.lookAt(0, 0, -1000);
 
 import PlaneModel from "./assets/models/airplane/Enemy_Plane_box.glb";
-import BulletModel from "./assets/models/bullet/bullet.glb";
+import BulletModel from "./assets/models/bullet/untitled.glb";
 
 /**
  * Plane class for managing the plane model
@@ -91,8 +91,9 @@ class Plane {
         console.log(`this.obj`, this.obj);
         // this.obj.rotateY(Math.PI);
         this.aR.copy(this.obj.quaternion);
-        // this.obj.children[0].visible = false;
         this.box = this.obj.children[0];
+        this.box.visible = false;
+        console.log(`this.box`, this.box);
         console.log(`this.obj`, this.obj);
         this.start();
       },
@@ -101,20 +102,6 @@ class Plane {
       },
       (error) => {
         console.error("Error Importing Plane Model", error);
-      }
-    );
-
-    loader.load(
-      BulletModel,
-      (gltf) => {
-        const object = gltf.scene || gltf.scenes[0];
-        this.bullet = object.clone();
-      },
-      (xhr) => {
-        console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
-      },
-      (error) => {
-        console.error("Error Importing Bullet Model", error);
       }
     );
   }
@@ -131,7 +118,6 @@ class Plane {
     this.parent.add(this.obj);
   }
 
-  shoot_bullet() {}
   // keep movement and rotation different
   // * movement would be strafing and forward speed is constant
   // * rotation would only be when we are moving
@@ -182,7 +168,7 @@ import Level1Model from "./assets/models/level/rainbow_road4.glb";
  * The rainbow road model
  */
 class LevelOne {
-  constructor() {
+  constructor(player) {
     this.scene = new Scene();
     // states ['unloaded', 'normal', 'shoot']
     this.state = {
@@ -192,7 +178,7 @@ class LevelOne {
 
     // Renderer settings
     renderer.shadowMap.enabled = false;
-
+    this.player = player;
     // Fog effect so that farther things are not seen
     const fog = new FogExp2(0xffffff, 0.003);
     this.scene.fog = fog;
@@ -219,9 +205,11 @@ class LevelOne {
     // Load the plane
     this.plane = new Plane(
       this.scene,
-      new Vector3(0, 0, 0),
+      new Vector3(0, 0, -10),
       new Vector3(12, 12, 24)
     );
+
+    this.bullets = [];
 
     this.update.bind(this);
     this.check_collision.bind(this);
@@ -234,6 +222,7 @@ class LevelOne {
       (gltf) => {
         const group = gltf.scene || gltf.scenes[0];
         this.obj = group.clone();
+        console.log(`level this.obj`, this.obj)
         this.start();
       },
       (xhr) => {
@@ -243,12 +232,27 @@ class LevelOne {
         console.error("Error Importing Level Model", error);
       }
     );
+
+    loader.load(
+      BulletModel,
+      (gltf) => {
+        const object = gltf.scene || gltf.scenes[0];
+        this.bullet = object.clone();
+        console.log(`this.bullet`, this.bullet);
+      },
+      (xhr) => {
+        console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
+      },
+      (error) => {
+        console.error("Error Importing Bullet Model", error);
+      }
+    );
   }
 
   start() {
     this.state.active = true;
     this.collidableMeshList = this.obj.children.filter((mesh, index, arr) => {
-      return mesh.name !== "tunnel" || mesh.name != "Plane";
+      return mesh.name != "tunnel" && mesh.name != "Plane";
     });
     console.log(`this.collidableMeshList`, this.collidableMeshList);
     // I would want to defer this so as to save memory
@@ -259,7 +263,6 @@ class LevelOne {
     if (this.plane.box == undefined) {
       return;
     }
-
     let temp = this.plane.box.geometry.attributes.position.array;
     let vertices = [];
     for (let i = 0; i < temp.length; i += 3) {
@@ -291,10 +294,44 @@ class LevelOne {
         });
       }
     }
+    
     let unique = [...new Set(obj)];
-    unique.forEach((object) => {
-      console.log(`object`, object)
+    for (let object of unique) {
+      if (object.name.startsWith("Cylinder")) {
+        this.player.state.health = Math.min(this.player.state.health + 5, 100);
+        object.visible = false;
+      } else {
+        this.player.gameover()
+      }
+    }
+    obj = []
+    this.bullets.forEach((bull) => {
+      let dir = new Vector3(0, 0, -1)
+      dir.applyQuaternion(bull.quaternion)
+      raycaster.set(bull.position, dir);
+      let collisionResults = raycaster.intersectObjects(
+        this.collidableMeshList,
+        true
+      );
+      if (collisionResults.length > 0) {
+        collisionResults.forEach((result) => {
+          if (result.distance < 1) {
+            obj.push(result.object);
+          }
+        });
+        // bull.visible = false
+      }
     })
+
+    unique = [...new Set(obj)];
+    // console.log(`object`, object)
+    for (let object of unique) {
+      if (object.name.startsWith("buffer-0-mesh")) {
+        console.log(`object`, object)
+        object.parent.visible = false;
+        this.scene.remove(object.parent)
+      }
+    }
   }
 
   update(dt) {
@@ -306,19 +343,32 @@ class LevelOne {
     this.plane.update(dt);
 
     if (this.plane.state.active) {
-      // if (this.plane.obj.position.z >= -50) {
-      //   this.glitchPass.goWild = true;
-      // } else
-      if (this.plane.obj.position.z <= -2000) {
+      if (this.plane.obj.position.z >= -50) {
+        this.glitchPass.goWild = true;
+      } else if (this.plane.obj.position.z <= -2000) {
         this.end();
         return;
+      } else if (this.plane.obj.position.z <= -1900) {
+        this.glitchPass.goWild = true;
+      } else {
+        this.glitchPass.goWild = false;
       }
-      //  else if (this.plane.obj.position.z <= -1900) {
-      //   this.glitchPass.goWild = true;
-      // } else {
-      //   this.glitchPass.goWild = false;
-      // }
     }
+
+    // Release the bullet
+    if (keys[" "] && this.player.state.bullets) {
+      let bull = this.bullet.clone();
+      bull.position.addVectors(
+        this.plane.obj.position,
+        new Vector3(0, 0, -1).applyQuaternion(this.plane.obj.quaternion)
+      );
+      bull.quaternion.copy(this.plane.obj.quaternion);
+      this.bullets.push(bull);
+      this.player.state.bullets -= 1
+      this.scene.add(bull);
+      keys[" "] = false;
+    }
+
     // Update the objects position
     let ufoSpeed = 12;
     let objSpeed = 1;
@@ -332,19 +382,31 @@ class LevelOne {
           mesh.position.add(new Vector3(0, 0, dt * objSpeed));
         }
       } else if (mesh.name === "tunnel") {
-        mesh.rotateZ(dt / 4);
+        mesh.rotateZ(dt / 2);
+      }
+    });
+    this.bullets.forEach((bullet) => {
+      bullet.translateZ(-30 * dt);
+      if (bullet.position.distanceTo(this.plane.obj.position) >= 100) {
+        this.scene.remove(bullet);
       }
     });
 
     // Do collision logic
     this.check_collision();
+    this.bullets = this.bullets.filter((bull) => {
+      return bull.visible && bull.position.distanceTo(this.plane.obj.position) < 100;
+    });
+
+    this.collidableMeshList = this.collidableMeshList.filter((obj) => {
+      return obj.visible
+    })
+
+    this.player.state.distance = -1 * Math.round(this.plane.obj.position.z);
 
     // Update Camera position
     if (this.plane.state.active) {
-      camera.position.addVectors(
-        this.plane.obj.position,
-        new Vector3(0, 0, 12)
-      );
+      camera.position.addVectors(this.plane.obj.position, new Vector3(0, 2, 8));
     }
   }
 
@@ -496,18 +558,33 @@ class Hud {
     this.hud.height = canvas.height;
     this.ctx = this.hud.getContext("2d");
     this.player = player;
-    // this.ctx.font = "30px Comic Sans MS";
-    // this.ctx.fillStyle = "red";
-    // this.ctx.textAlign = "center";
-    // this.ctx.fillText("Hello World", canvas.width / 2, canvas.height / 2);
-    // this.ctx.textAlign = "left";
-    // this.ctx.fillText("Hello World", 1, 30);
+
+    this.font = new FontFace('gamery', 'url("https://fonts.gstatic.com/s/pressstart2p/v9/e3t4euO8T-267oIAQAu6jDQyK3nVivM.woff2")')
+
+    this.font.load().then((font) => {
+      document.fonts.add(font);
+      this.ctx.font = "bold 18px gamery"
+    });
+
+    this.overlay = document.createElement("div");
+    this.overlay.style.zIndex =11;
+    this.overlay.style.position = "absolute";
+    this.overlay.style.right = "10px";
+    this.overlay.style.top = "10px";
+    this.overlay.style.padding = "10px";
+    this.overlay.style.backgroundColor = "white";
+    this.overlay.style.borderRadius = "10px";
+    this.overlay.style.maxWidth = "500px";
+    this.overlay.style.lineHeight = 1.2;
+
     this.clear();
+    document.body.appendChild(this.overlay);
     document.body.appendChild(this.hud);
     setInterval(this.update.bind(this), 100);
   }
 
   clear() {
+    
     this.ctx.clearRect(0, 0, this.hud.width, this.hud.height);
   }
 
@@ -532,15 +609,60 @@ class Hud {
     this.ctx.fill();
   }
 
-  show_bullet_count() {}
+  show_bullet_count() {
+    this.ctx.fillStyle = "white";
+    this.ctx.fillText("Bullets:" + this.player.state.bullets.toString(), 10, 54)
+  }
 
-  show_distance() {}
+  show_distance() {
+    this.ctx.fillStyle = "white";
+    this.ctx.fillText("Distance:" + this.player.state.distance.toString() + "/1000", 10, 28)
+  }
 
-  show_tooltips(pos) {}
+  show_tooltips() {
+    if (this.player.state.level == 1) {
+      if (this.player.state.distance < 200) {
+        this.overlay.innerHTML =
+          "What just happened?!?! You seem to be lost in the quantum realm, there is only only way - forward. Better not get in contact with things here. Let's keep moving.";
+      } else if (this.player.state.distance < 400) {
+        this.overlay.innerHTML =
+          "Pick up the energy tokens to replenish your energy, if your energy get's to zero you'll be stuck here forever.";
+      } else if (this.player.state.distance < 800) {
+        this.overlay.innerHTML =
+          "These aliens seem to coming after you. Use your bullets to destroy them.";
+      } else if (this.player.state.distance < 1200) {
+        this.overlay.innerHTML =
+          "Don't mind me, I am just a voice in your head... Look out!! that eight ball is coming right at you ";
+      } else if (this.player.state.distance < 1900) {
+        this.overlay.innerHTML = "Fight the aliens and reach the portal.";
+      } else {
+        this.overlay.innerHTML = "Pzzt Pzzt I seem to be loosing connection";
+      }
+    } else {
+      if (this.player.state.distance < 200) {
+        this.overlay.innerHTML =
+          "You seem to have reached the alien hive, there is only only way - forward. The crystals seem dangerous better not get in contact with them.";
+      } else if (this.player.state.distance < 600) {
+        this.overlay.innerHTML =
+          "These aliens seem to be coming from the end of the hive. Maybe there is a way out?";
+      } else {
+        this.overlay.innerHTML = "Fight the aliens and reach the portal.";
+      }
+    }
+  }
 
   update() {
     this.clear();
-    this.show_health_bar();
+    if (!this.player.state.gameover) {
+      this.show_bullet_count()
+      this.show_health_bar();
+      this.show_tooltips();
+      this.show_distance();
+    } else {
+      this.ctx.fillStyle = "black";
+      this.ctx.fillRect(0, 0, this.hud.width, this.hud.height);
+      this.overlay.innerHTML = "Game Over you lost";
+    }
   }
 }
 
@@ -550,20 +672,28 @@ export class Player {
       level: 1,
       active: false,
       health: 100,
-      bullets: 0,
+      bullets: 10,
       distance: 0,
+      gameover: false
     };
+  }
+
+  gameover() {
+    this.state.gameover = true
+    cancelAnimationFrame(FRAME);
   }
 
   start() {
     this.state.active = true;
-    this.level = new LevelOne();
+    this.level = new LevelOne(this);
   }
 
   next_level() {
     this.state.health = 100;
+    this.state.bullets = 10;
     this.state.level = 2;
-    this.level = new LevelTwo();
+    this.state.distance = 0;
+    this.level = new LevelTwo(this);
   }
 
   update() {
@@ -573,7 +703,7 @@ export class Player {
       if (this.state.level === 1) {
         this.next_level();
       } else {
-        cancelAnimationFrame(FRAME);
+        this.gameover()
       }
     }
   }
@@ -593,7 +723,7 @@ const render = () => {
 };
 
 const animate = () => {
-  FRAME = requestAnimationFrame(animate);  
+  FRAME = requestAnimationFrame(animate);
   // controls.update();
   render();
 };
